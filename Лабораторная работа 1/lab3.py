07e4d3eace73b356144adced5ecfab47fd471456
Nika Kolomiychenko <nikakolomiychenko6@gmail.com>
2024-10-23 23:31:46 +0300

    Add files via upload

---
+++ Лабораторная работа 1/lab3.py
@@ -0,0 +1,10 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+
+mid = len(list_players) // 2 #находим середину списку
+
+team1 = list_players[:mid]
+team2 = list_players[mid:]
+
+print(team1)
+print(team2)
