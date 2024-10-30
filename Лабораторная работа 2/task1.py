981433fe40ccfafd3e6f6373ef8febb4f282300a
Nika Kolomiychenko <nikakolomiychenko6@gmail.com>
2024-10-30 21:28:57 +0300

    Add files via upload

---
+++ Лабораторная работа 2/task1.py
@@ -0,0 +1,20 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+
+# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
+
+month = 0
+while True:
+    diff = spend - salary
+    if diff > money_capital:
+        break
+    money_capital -= diff
+    spend += spend * increase
+    month += 1
+print("Количество месяцев, которое можно протянуть без долгов:", month)
+
+
+
+
